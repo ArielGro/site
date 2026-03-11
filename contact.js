9f45d1f531a3584cb4be3ea4d21369bd8ce05a7a
ArielGro <groariel@gmail.com>
2026-03-11 16:03:47 +0200

    Task4. Contact form
    Fixed bugs with the form

--- contact.js
+++ contact.js
@@ -5,9 +5,22 @@ function validateForm() {
     const phone = document.getElementById("Tel").value;
     const issue = document.getElementById("Issue").value;
 
+
+    const usernamePattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_]+$/;
+
     if (username.trim() === "") {
         alert("Please enter a username!");
         return; 
+    }
+
+    if( username.length < 3 || username.length > 20) {
+        alert("Username must be between 3 and 20 characters!");
+        return;
+    }
+
+    if(!usernamePattern.test(username)) {
+        alert("The username must contain one Cap letter, one small letter, and one number.");
+        return;
     }
 
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -16,14 +29,20 @@ function validateForm() {
         return;
     }
 
-    if (age < 15 || age > 120) {
-        alert("Age must be between 15 and 120!");
+    const numberCheck = /[0-9]/;
+    if (numberCheck.test(email)) {
+        alert("Your email cannot contain numbers! Letters only, please.");
         return;
     }
 
-    const phonePattern = /^[0-9]{10}$/; 
+    if (age < 15 || age >= 120) {
+        alert("Age must be at least 15 and less than 120!");
+        return;
+    }
+
+    const phonePattern = /^[05][0-9]{9}$/; 
     if (!phonePattern.test(phone)) {
-        alert("Please enter a valid 10-digit phone number (e.g., 0501234567)!");
+        alert("Please enter a valid phone number (e.g., 0501234567)!");
         return;
     }
 
